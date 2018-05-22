e99fe890df048998abb3ed3d82eba370c970d6d1
Caleb Phillips <calebrphillips@gmail.com>
2018-05-22 15:23:04 -0400

    Incorporate sensors together and Settle on an output format

--- RD117_ARDUINO.ino
+++ RD117_ARDUINO.ino
@@ -33,15 +33,10 @@
 #include <SPI.h>
 #include "algorithm_by_RF.h"
 #include "max30102.h"
-
-#define DEBUG // Uncomment for debug output to the Serial stream
-
+#include <MPU9250_asukiaaa.h>
 
 // ADALOGGER pins
 const byte chipSelect = 4;
-const byte cardDetect = 7;
-const byte batteryPin = 9;
-const byte ledPin = 13; // Red LED on ADALOGGER
 const byte oxiInt = 10; // ADALOGGER pin connected to MAX30102 INT
 
 uint32_t elapsedTime,timeStart;
@@ -51,36 +46,37 @@ uint32_t aun_red_buffer[BUFFER_SIZE];  //red LED sensor data
 float old_n_spo2;  // Previous SPO2 value
 uint8_t uch_dummy,k;
 
+//MPU9250 Vars
+MPU9250 mySensor;
+
+float aX, aY, aZ, aSqrt, gX, gY, gZ;
+
+//Output vars
+String led_output, accel_output, gyro_output;
+
 void setup() {
 
-  pinMode(cardDetect,INPUT_PULLUP);
-  pinMode(batteryPin,INPUT);
   pinMode(oxiInt, INPUT);  //pin D10 connects to the interrupt output pin of the MAX30102
-  pinMode(ledPin,OUTPUT);
-  digitalWrite(ledPin,LOW);
-
   Wire.begin();
-
-#ifdef DEBUG
-  // initialize serial communication at 115200 bits per second:
+  
   Serial.begin(115200);
-#endif
 
   maxim_max30102_reset(); //resets the MAX30102
   delay(1000);
 
   maxim_max30102_read_reg(REG_INTR_STATUS_1,&uch_dummy);  //Reads/clears the interrupt status register
 
-    // Measure battery voltage
-  float measuredvbat = analogRead(batteryPin);
-  measuredvbat *= 2;    // we divided by 2, so multiply back
-  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
-  measuredvbat /= 1024; // convert to voltage
-
   char my_status[20];
   
   old_n_spo2=0.0;
   maxim_max30102_init();  //initialize the MAX30102
+
+  //SETUP MPU9250
+  mySensor.setWire(&Wire);
+  mySensor.beginAccel();
+  mySensor.beginGyro();
+  //mySensor.beginMag();
+
 }
 
 //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 4 seconds
@@ -91,86 +87,69 @@ void loop() {
   int8_t  ch_hr_valid;  //indicator to show if the heart rate calculation is valid
   int8_t i;
   char hr_str[10];
-     
+  
   //buffer length of 100 stores 4 seconds of samples running at 25sps
   //read 100 samples, and determine the signal range
+  timeStart = millis();
+  Serial.println("start");
   for(i=0;i<BUFFER_SIZE;i++)
   {
+    //Get LED Values
     while(digitalRead(oxiInt)==1);  //wait until the interrupt pin asserts
     maxim_max30102_read_fifo((aun_red_buffer+i), (aun_ir_buffer+i));  //read from MAX30102 FIFO
-#ifdef DEBUG
-    Serial.print(i, DEC);
-    Serial.print(F("\t"));
-    Serial.print(aun_red_buffer[i], DEC);
-    Serial.print(F("\t"));
-    Serial.print(aun_ir_buffer[i], DEC);    
-    Serial.println("");
-#endif
+    
+    elapsedTime=millis()-timeStart;
+    Serial.println("offset " + String(elapsedTime, DEC));
+    
+    led_output = "led ";
+    led_output += " ";
+    led_output += String(aun_red_buffer[i], DEC);
+    led_output += " ";
+    led_output += String(aun_ir_buffer[i], DEC);    
+    Serial.println(led_output);
+
+    //Get Accel Values
+    elapsedTime=millis()-timeStart;
+    accel_output = "accel ";
+    mySensor.accelUpdate();
+    aX = mySensor.accelX();
+    aY = mySensor.accelY();
+    aZ = mySensor.accelZ();
+    aSqrt = mySensor.accelSqrt();
+    accel_output += String(aX);
+    accel_output += " ";
+    accel_output += String(aY);
+    accel_output += " ";
+    accel_output += String(aZ);
+    accel_output += " ";
+    accel_output += String(aSqrt);
+    Serial.println(accel_output);
+
+    elapsedTime=millis()-timeStart;
+    gyro_output = "gyro ";
+    mySensor.gyroUpdate();
+    gX = mySensor.gyroX();
+    gY = mySensor.gyroY();
+    gZ = mySensor.gyroZ();
+    gyro_output += String(gX);
+    gyro_output += " ";
+    gyro_output += String(gY);
+    gyro_output += " ";
+    gyro_output += String(gZ);
+    Serial.println(gyro_output);
   }
 
   //calculate heart rate and SpO2 after 100 samples (4 seconds of samples) using Robert's method
   rf_heart_rate_and_oxygen_saturation(aun_ir_buffer, BUFFER_SIZE, aun_red_buffer, &n_spo2, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid, &ratio, &correl); 
-  elapsedTime=millis()-timeStart;
-  millis_to_hours(elapsedTime,hr_str); // Time in hh:mm:ss format
-  elapsedTime/=1000; // Time in seconds
 
-#ifdef DEBUG
-  Serial.println("--RF--");
-  Serial.print(elapsedTime);
-  Serial.print("\t");
-  Serial.print(n_spo2);
-  Serial.print("\t");
-  Serial.print(n_heart_rate, DEC);
-  Serial.print("\t");
-  Serial.println(hr_str);
-  Serial.println("------");
-#endif
+  Serial.print("oxygen ");
+  Serial.println(n_spo2);
+  
+  Serial.print("hr");
+  Serial.println(n_heart_rate, DEC);
 
- 
   if(ch_hr_valid && ch_spo2_valid) { 
     old_n_spo2=n_spo2;
-  }
-}
-
-void millis_to_hours(uint32_t ms, char* hr_str)
-{
-  char istr[6];
-  uint32_t secs,mins,hrs;
-  secs=ms/1000; // time in seconds
-  mins=secs/60; // time in minutes
-  secs-=60*mins; // leftover seconds
-  hrs=mins/60; // time in hours
-  mins-=60*hrs; // leftover minutes
-  itoa(hrs,hr_str,10);
-  strcat(hr_str,":");
-  itoa(mins,istr,10);
-  strcat(hr_str,istr);
-  strcat(hr_str,":");
-  itoa(secs,istr,10);
-  strcat(hr_str,istr);
-}
-
-// blink three times if isOK is true, otherwise blink continuously
-void blinkLED(const byte led, bool isOK)
-{
-  byte i;
-  if(isOK) {
-    for(i=0;i<3;++i) {
-      digitalWrite(led,HIGH);
-      delay(200);
-      digitalWrite(led,LOW);
-      delay(200);
-    }
-  } else {
-    while(1) {
-      for(i=0;i<2;++i) {
-        digitalWrite(led,HIGH);
-        delay(50);
-        digitalWrite(led,LOW);
-        delay(50);
-      }
-      delay(500);
-    }
   }
 }
 
