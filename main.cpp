90a6fd6b67172ca60dc0a76157884479dbff42b6
Caleb Phillips <calebrphillips@gmail.com>
2018-06-27 13:41:36 -0400

    Parse expected output string from clojure subprocess call and put in expected input format of library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
+#include <assert.h> 
 
 #include "algorithm_by_RF.h"
 #include "max30102.h"
@@ -8,9 +11,47 @@
 
 using namespace std;
 
+const int sample_size = 100;
+
+uint32_t red[sample_size];
+uint32_t ir[sample_size];
+
+void convert_ss_to_array(istringstream *ss, uint32_t *output) {
+  uint32_t val;
+  int index = 0;
+
+
+  while (*ss >> val) {
+    output[index++] = val;
+
+    if (ss->peek() == ',')
+      ss->ignore();
+  }
+  cout << index << endl;
+  assert (index == sample_size);
+}
+
 int main() {
-  string line;
+  string line, red_line, ir_line;
   getline(cin, line);
-  cout << line << endl;
+
+  string s = "split on    whitespace   ";
+  vector<string> result;
+
+  istringstream liness(line);
+
+  liness >> red_line;
+  liness >> ir_line;
+
+  istringstream redss(red_line), irss(ir_line);
+
+  convert_ss_to_array(&redss, red);
+  convert_ss_to_array(&irss, ir);
+
+  for (int i = 0; i < sample_size; i++) {
+    cout << red[i] << " " << ir[i] << endl;
+  }
+
   return 0;
- }
+}
+
